5aa10bafdfb6652c5e2e27a68f416de03eeac710
okaflocka <oka.simian@gmail.com>
2022-09-14 12:37:09 +0900

    priceバリデーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,7 +3,7 @@ class Item < ApplicationRecord
   validates :image, :title, :content, presence: true
   validates :category_id, :condition_id, :shipping_fee_id, :shipping_days_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :shipping_area_id, numericality: { other_than: 0 , message: "can't be blank"}
-  validates :price, presence: true, numericality: { only_integer: true }
+  validates :price, presence: true, numericality: { only_integer: true , greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
 
   belongs_to :user
   has_one_attached :image
