f57557da5549463d0af262a8437fdd8f57ba25a3
okaflocka <oka.simian@gmail.com>
2022-09-19 17:07:25 +0900

    配送情報バリデーション・エラー表示実装

--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -2,11 +2,12 @@ class PurchaseAddress
   include ActiveModel::Model
   attr_accessor :user_id, :item_id, :zipcode, :prefecture_id, :city, :address_num, :name_building, :phone_number
 
-  #with_options presence: true do
-    #validates :user_id, :item_id, :city, :address_num
-    #validates :zipcode, format
-    #validates :phone_number, format 
-  #end
+  with_options presence: true do
+    validates :user_id, :item_id, :city, :address_num
+    validates :prefecture_id, numericality: { other_than: 0 , message: "can't be blank"}
+    validates :zipcode, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :phone_number, format: {with: /\A[0-9]{10,11}\z/} 
+  end
 
   def save
     purchase = Purchase.create(user_id: user_id, item_id: item_id)
