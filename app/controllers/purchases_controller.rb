cb56a6c6831a35ed0bb7792120b6f4d1e878a64f
okaflocka <oka.simian@gmail.com>
2022-09-20 14:45:50 +0900

    クレジットカード決済機能実装

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -11,19 +11,31 @@ class PurchasesController < ApplicationController
   end
 
   def create
+    @item = Item.find(params[:item_id])
     @purchase_address = PurchaseAddress.new(purchase_params)
     if @purchase_address.valid?
+      payjp
       @purchase_address.save
       redirect_to root_path
     else
-      @item = Item.find(params[:item_id])
       render :index
     end
   end
 
   private
   def purchase_params
-    params.require(:purchase_address).permit(:zipcode, :prefecture_id, :city, :address_num, :name_building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+    params.require(:purchase_address)
+    .permit(:zipcode, :prefecture_id, :city, :address_num,:name_building, :phone_number)
+    .merge(user_id: current_user.id, item_id: params[:item_id])
+  end
+
+  def payjp
+    Payjp.api_key = "sk_test_ddc94aeee887dee44dd89621" 
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: params[:token], 
+      currency: 'jpy'
+    )
   end
 
 end
--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -1,9 +1,9 @@
 class PurchaseAddress
   include ActiveModel::Model
-  attr_accessor :user_id, :item_id, :zipcode, :prefecture_id, :city, :address_num, :name_building, :phone_number
+  attr_accessor :user_id, :item_id, :zipcode, :prefecture_id, :city, :address_num, :name_building, :phone_number, :token
 
   with_options presence: true do
-    validates :user_id, :item_id, :city, :address_num
+    validates :user_id, :item_id, :city, :address_num, :token
     validates :prefecture_id, numericality: { other_than: 0 , message: "can't be blank"}
     validates :zipcode, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :phone_number, format: {with: /\A[0-9]{10,11}\z/} 
